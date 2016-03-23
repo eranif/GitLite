d57da95e4e53a1f88e8410a82fd59c19d02846bf
Eran <eran.ifrah@gmail.com>
2016-03-23 10:06:36 +0200

    Support for reading/writing JSON into and from file

--- GitLite/wxSimpleJSON.cpp
+++ GitLite/wxSimpleJSON.cpp
@@ -1,5 +1,6 @@
 #include "wxSimpleJSON.h"
 #include "cJSON.h"
+#include <wx/ffile.h>
 
 static cJSON* cJSONAllocNew()
 {
@@ -26,9 +27,18 @@ wxSimpleJSON::Ptr_t wxSimpleJSON::Create(wxSimpleJSON::eType type, bool isRoot)
     return ptr;
 }
 
+wxSimpleJSON::Ptr_t wxSimpleJSON::Create(cJSON* p, bool canDelete)
+{
+    wxSimpleJSON* obj = new wxSimpleJSON();
+    obj->m_d = p;
+    obj->m_canDelete = canDelete;
+    wxSimpleJSON::Ptr_t ptr(obj, wxSimpleJSON::Destroy);
+    return ptr;
+}
+
 void wxSimpleJSON::Destroy(wxSimpleJSON* obj)
 {
-    if(obj->m_canDelete) {
+    if(obj->m_canDelete && obj->m_d) {
         cJSON_Delete(obj->m_d);
     }
     obj->m_d = NULL;
@@ -87,4 +97,115 @@ wxSimpleJSON& wxSimpleJSON::Add(const wxString& name, const wxArrayString& arr, 
         parr->ArrayAdd(arr.Item(i), conv);
     }
     return Add(name, parr);
+}
+
+size_t wxSimpleJSON::ArraySize() const { return cJSON_GetArraySize(m_d); }
+
+wxSimpleJSON::Ptr_t wxSimpleJSON::Item(size_t index) const
+{
+    if(!m_d || (m_d->type != cJSON_Array)) {
+        return Create((cJSON*)NULL);
+    }
+    cJSON* item = cJSON_GetArrayItem(m_d, index);
+    return Create(item);
+}
+
+wxString wxSimpleJSON::GetValueString(const wxMBConv& conv) const
+{
+    if(!m_d || (m_d->type != cJSON_String)) {
+        return wxEmptyString;
+    }
+    return wxString(m_d->valuestring, conv);
+}
+
+wxArrayString wxSimpleJSON::GetValueArrayString(const wxMBConv& conv) const
+{
+    if(!m_d || (m_d->type != cJSON_Array)) {
+        return wxArrayString();
+    }
+
+    wxArrayString arr;
+    wxSimpleJSON::Ptr_t parr = Create(m_d);
+    for(size_t i = 0; i < parr->ArraySize(); ++i) {
+        arr.Add(parr->Item(i)->GetValueString(conv));
+    }
+    return arr;
+}
+
+double wxSimpleJSON::GetValueNumber(double defaultValue) const
+{
+    if(!m_d || (m_d->type != cJSON_Number)) {
+        return defaultValue;
+    }
+    return m_d->valuedouble;
+}
+
+wxSimpleJSON::Ptr_t wxSimpleJSON::GetProperty(const wxString& name) const
+{
+    if(!m_d || (m_d->type != cJSON_Object)) {
+        return Create(NULL);
+    }
+    return Create(cJSON_GetObjectItem(m_d, name.mb_str(wxConvUTF8).data()));
+}
+
+wxSimpleJSON& wxSimpleJSON::ArrayAdd(bool value)
+{
+    if(!m_d || (m_d->type != cJSON_Array)) {
+        return *this;
+    }
+    if(value) {
+        cJSON_AddItemToArray(m_d, cJSON_CreateTrue());
+    } else {
+        cJSON_AddItemToArray(m_d, cJSON_CreateFalse());
+    }
+    return *this;
+}
+
+wxSimpleJSON& wxSimpleJSON::Add(const wxString& name, bool value)
+{
+    if(!m_d || (m_d->type != cJSON_Object)) {
+        return *this;
+    }
+    cJSON_AddBoolToObject(m_d, name.mb_str(wxConvUTF8).data(), value);
+    return *this;
+}
+
+bool wxSimpleJSON::GetValueBool(bool defaultValue) const
+{
+    if(!m_d || m_d->type != cJSON_True || m_d->type != cJSON_False) {
+        return defaultValue;
+    }
+    // at this point, type can only be cJSON_True or cJSON_False
+    return m_d->type == cJSON_True;
+}
+
+wxSimpleJSON::Ptr_t wxSimpleJSON::Create(const wxString& buffer, const wxMBConv& conv)
+{
+    cJSON* p = cJSON_Parse(buffer.mb_str(conv).data());
+    return Create(p, true);
+}
+
+wxSimpleJSON::Ptr_t wxSimpleJSON::Create(const wxFileName& filename, const wxMBConv& conv)
+{
+    if(!filename.Exists()) {
+        return Create(NULL);
+    }
+    wxFFile fp(filename.GetFullPath(), "rb");
+    wxString content;
+    if(fp.IsOpened() && fp.ReadAll(&content, conv)) {
+        fp.Close();
+        return Create(content, conv);
+    }
+    return Create(NULL);
+}
+
+bool wxSimpleJSON::Save(const wxFileName& filename, const wxMBConv& conv)
+{
+    wxFFile fp(filename.GetFullPath(), "wb");
+    if(fp.IsOpened()) {
+        fp.Write(Print(true, conv), conv);
+        fp.Close();
+        return true;
+    }
+    return false;
 }
--- GitLite/wxSimpleJSON.h
+++ GitLite/wxSimpleJSON.h
@@ -5,6 +5,7 @@
 #include "cJSON.h"
 #include <wx/string.h>
 #include <wx/arrstr.h>
+#include <wx/filename.h>
 
 class wxSimpleJSON
 {
@@ -25,40 +26,98 @@ public:
         kObject, // 6
     };
     virtual ~wxSimpleJSON();
+    typedef wxSharedPtr<wxSimpleJSON> Ptr_t;
 
 private:
     // Constructor is private. The way to create an object is by using the ::Create method
     wxSimpleJSON();
-    
+
     /**
      * @brief the deleter pointer
      */
     static void Destroy(wxSimpleJSON* obj);
-
-public:
-    typedef wxSharedPtr<wxSimpleJSON> Ptr_t;
+    static wxSimpleJSON::Ptr_t Create(cJSON* p, bool canDelete = false);
 
 public:
     // Custom object generators
     /**
      * @brief create a new JSON node.
      * @param type the node type
-     * @param isRoot
-     * @return
+     * @param isRoot pass this when creating the top level item (the root item of the JSON)
+     * @note Check for IsNull()
      */
     static wxSimpleJSON::Ptr_t Create(wxSimpleJSON::eType type, bool isRoot = false);
+
+    /**
+     * @brief parse and reutrn wxSimpleJSON object
+     * @note Check for IsNull()
+     */
+    static wxSimpleJSON::Ptr_t Create(const wxString& buffer, const wxMBConv& conv = wxConvUTF8);
+
+    /**
+     * @brief read JSON content from a file, parse and reutrn wxSimpleJSON object
+     * @note Check for IsNull()
+     */
+    static wxSimpleJSON::Ptr_t Create(const wxFileName& filename, const wxMBConv& conv = wxConvUTF8);
+    
+    /**
+     * @brief save the content of this object to a file
+     */
+    bool Save(const wxFileName& filename, const wxMBConv& conv = wxConvUTF8);
+    
+    /**
+     * @brief is this object NULL?
+     */
+    inline bool IsNull() const { return (m_d == NULL); }
 
     // Array manipulation
     wxSimpleJSON& ArrayAdd(wxSimpleJSON::Ptr_t obj);
     wxSimpleJSON& ArrayAdd(const wxString& value, const wxMBConv& conv = wxConvUTF8);
     wxSimpleJSON& ArrayAdd(double value);
+    wxSimpleJSON& ArrayAdd(bool value);
     wxSimpleJSON& ArrayAdd(const wxArrayString& arr, const wxMBConv& conv = wxConvUTF8);
+
+    /**
+     * @brief return the array size. Returns 0 if this object is not an array
+     */
+    size_t ArraySize() const;
+    /**
+     * @brief return array as a given position
+     * @param index item position in the array
+     * @return valid wxSimpleJSON::Ptr_t obj is a Null object (call wxSimpleJSON::IsNull() to test for null)
+     */
+    wxSimpleJSON::Ptr_t Item(size_t index) const;
 
     // Object manipulation
     wxSimpleJSON& Add(const wxString& name, wxSimpleJSON::Ptr_t obj);
     wxSimpleJSON& Add(const wxString& name, const wxString& value, const wxMBConv& conv = wxConvUTF8);
     wxSimpleJSON& Add(const wxString& name, double value);
+    wxSimpleJSON& Add(const wxString& name, bool value);
     wxSimpleJSON& Add(const wxString& name, const wxArrayString& arr, const wxMBConv& conv = wxConvUTF8);
+
+    /**
+     * @brief return the string value
+     */
+    wxString GetValueString(const wxMBConv& conv = wxConvUTF8) const;
+
+    /**
+     * @brief return the string value
+     */
+    bool GetValueBool(bool defaultValue = false) const;
+
+    /**
+     * @brief return the array string value
+     */
+    wxArrayString GetValueArrayString(const wxMBConv& conv = wxConvUTF8) const;
+    /**
+     * @brief return the number value
+     */
+    double GetValueNumber(double defaultValue = -1) const;
+
+    /**
+     * @brief return an object property
+     */
+    wxSimpleJSON::Ptr_t GetProperty(const wxString& name) const;
 
     /**
      * @brief convert this JSON object to string
@@ -66,7 +125,7 @@ public:
      * @param conv string conversion object
      * @return JSON object as string
      */
-    inline wxString ToString(bool pretty = true, const wxMBConv& conv = wxConvUTF8) const
+    inline wxString Print(bool pretty = true, const wxMBConv& conv = wxConvUTF8) const
     {
         char* b = pretty ? cJSON_Print(m_d) : cJSON_PrintUnformatted(m_d);
         wxString s(b, conv);
